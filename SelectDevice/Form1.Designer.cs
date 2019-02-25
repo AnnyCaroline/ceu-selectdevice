8e227da1ff72a5e15a02fee21e06b25bdb823b49
AnnyCaroline <annycarolinegnr@gmail.com>
2019-02-25 01:54:36 -0300

    initial commit

---
+++ SelectDevice/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace SelectDevice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtBoard = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCpu = new System.Windows.Forms.Label();
+            this.comboCPU = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnIDE = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.comboMCU = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Port = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.board = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mcu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cpu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Port,
+            this.board,
+            this.Name1,
+            this.PID,
+            this.VID,
+            this.SN,
+            this.mcu,
+            this.cpu});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 38);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(694, 169);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Enabled = false;
+            this.txtPort.Location = new System.Drawing.Point(394, 252);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(79, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // txtBoard
+            // 
+            this.txtBoard.Enabled = false;
+            this.txtBoard.Location = new System.Drawing.Point(479, 252);
+            this.txtBoard.Name = "txtBoard";
+            this.txtBoard.Size = new System.Drawing.Size(100, 20);
+            this.txtBoard.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(391, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(476, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Selected board";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(594, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&Continue";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCpu
+            // 
+            this.lblCpu.AutoSize = true;
+            this.lblCpu.Location = new System.Drawing.Point(264, 232);
+            this.lblCpu.Name = "lblCpu";
+            this.lblCpu.Size = new System.Drawing.Size(67, 13);
+            this.lblCpu.TabIndex = 9;
+            this.lblCpu.Text = "Select a cpu";
+            // 
+            // comboCPU
+            // 
+            this.comboCPU.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboCPU.FormattingEnabled = true;
+            this.comboCPU.Location = new System.Drawing.Point(267, 251);
+            this.comboCPU.Name = "comboCPU";
+            this.comboCPU.Size = new System.Drawing.Size(121, 21);
+            this.comboCPU.TabIndex = 2;
+            this.comboCPU.SelectedIndexChanged += new System.EventHandler(this.comboCPU_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Select the device";
+            // 
+            // btnIDE
+            // 
+            this.btnIDE.Location = new System.Drawing.Point(26, 232);
+            this.btnIDE.Name = "btnIDE";
+            this.btnIDE.Size = new System.Drawing.Size(54, 42);
+            this.btnIDE.TabIndex = 12;
+            this.btnIDE.Text = "Use IDE configurations";
+            this.btnIDE.UseVisualStyleBackColor = true;
+            this.btnIDE.Visible = false;
+            this.btnIDE.Click += new System.EventHandler(this.btnIDE_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Image = global::SelectDevice.Properties.Resources.refresh1;
+            this.btnRefresh.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRefresh.Location = new System.Drawing.Point(642, 8);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(78, 24);
+            this.btnRefresh.TabIndex = 13;
+            this.btnRefresh.Text = "&Refresh";
+            this.btnRefresh.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboMCU
+            // 
+            this.comboMCU.FormattingEnabled = true;
+            this.comboMCU.Location = new System.Drawing.Point(140, 251);
+            this.comboMCU.Name = "comboMCU";
+            this.comboMCU.Size = new System.Drawing.Size(121, 21);
+            this.comboMCU.TabIndex = 14;
+            this.comboMCU.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "mcu";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Port
+            // 
+            this.Port.HeaderText = "Port";
+            this.Port.Name = "Port";
+            this.Port.ReadOnly = true;
+            // 
+            // board
+            // 
+            this.board.HeaderText = "board";
+            this.board.Name = "board";
+            this.board.ReadOnly = true;
+            this.board.Visible = false;
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "Name";
+            this.Name1.Name = "Name1";
+            this.Name1.ReadOnly = true;
+            this.Name1.Width = 200;
+            // 
+            // PID
+            // 
+            this.PID.HeaderText = "PID";
+            this.PID.Name = "PID";
+            this.PID.ReadOnly = true;
+            // 
+            // VID
+            // 
+            this.VID.HeaderText = "VID";
+            this.VID.Name = "VID";
+            this.VID.ReadOnly = true;
+            // 
+            // SN
+            // 
+            this.SN.HeaderText = "SN";
+            this.SN.Name = "SN";
+            this.SN.ReadOnly = true;
+            this.SN.Width = 150;
+            // 
+            // mcu
+            // 
+            this.mcu.HeaderText = "mcu";
+            this.mcu.Name = "mcu";
+            this.mcu.ReadOnly = true;
+            // 
+            // cpu
+            // 
+            this.cpu.HeaderText = "cpu";
+            this.cpu.Name = "cpu";
+            this.cpu.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(748, 285);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboMCU);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnIDE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboCPU);
+            this.Controls.Add(this.lblCpu);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoard);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Céu-maker: select device";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtBoard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCpu;
+        private System.Windows.Forms.ComboBox comboCPU;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnIDE;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ComboBox comboMCU;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Port;
+        private System.Windows.Forms.DataGridViewTextBoxColumn board;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mcu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cpu;
+    }
+}
+
---
+++ SelectDevice/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Management;
+using System.Reflection;
+
+namespace SelectDevice
+{
+    public partial class Form1 : Form
+    {
+
+        private int selectedrowindex = -1;
+        private int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Helper function to handle regex search
+        private static string regex(string pattern, string text)
+        {
+            Regex re = new Regex(pattern);
+            Match m = re.Match(text);
+            if (m.Success)
+            {
+                return m.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void loadBoards()
+        {
+            string port = "", vid = "", pid = "", sn = "";
+
+            // Read file
+            try
+            {
+                // Use WMI to get info
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2",
+                "SELECT * FROM Win32_PnPEntity WHERE ClassGuid=\"{4d36e978-e325-11ce-bfc1-08002be10318}\"");
+
+                #if DEBUG
+                    string text = System.IO.File.ReadAllText(@"absolute-path-to\arduino-1.8.8\hardware\arduino\avr\boards.txt");
+                #else
+                    // get path of the executing assembly
+                    string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+<<<<<<< HEAD
+
+                    string text = System.IO.File.ReadAllText(currentPath + @"\..\arduino-1.8.8\hardware\arduino\avr\boards.txt");
+=======
+                    string text = System.IO.File.ReadAllText(currentPath + @"\..\arduino-1.8.3\hardware\arduino\avr\boards.txt");
+>>>>>>> 713f1f6cc0d047a0c5c711af70dadde1b55c8f58
+                #endif
+
+                this.dataGridView1.Rows.Clear();
+                this.txtBoard.Clear();
+                this.txtPort.Clear();
+                this.comboCPU.Items.Clear();
+
+                // Search all serial ports
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    // Parse the data
+                    if (null != queryObj["Name"])
+                    {
+                        //https://stackoverflow.com/questions/24135006/regex-that-match-any-character-inside-a-parenthesis
+                        port = regex("(?<=" + Regex.Escape("(") + ")[^)]*(?=" + Regex.Escape(")") + ")", queryObj["Name"].ToString());
+                    }
+
+                    if (null != queryObj["PNPDeviceID"])
+                    {
+                        //https://stackoverflow.com/questions/3926451/how-to-match-but-not-capture-part-of-a-regex?rq=1
+                        vid = regex("(?<=VID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
+                        pid = regex("(?<=PID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
+                        sn = regex("([0-9a-fA-F]{5,})", queryObj["PNPDeviceID"].ToString());
+
+                        if (!string.IsNullOrEmpty(port) && !string.IsNullOrEmpty(vid) && !string.IsNullOrEmpty(pid) && !string.IsNullOrEmpty(sn))
+                        {
+                            counter++;
+
+                            //Get the VID and PID lines
+                            string a = "";
+                            a = regex("[a-zA-Z]+" + Regex.Escape(".") + "vid" + Regex.Escape(".") + "[0-9]=0x" + vid + "((\r\n)|(\n))[a-zA-Z]+" + Regex.Escape(".") + "pid" + Regex.Escape(".") + "[0-9]=0x" + pid, text.ToString());
+
+                            //Get first word from a
+                            string b = "";
+                            b = regex("[a-zA-Z]+(?=" + Regex.Escape(".") + "vid)", a.ToString());
+
+                            //Get b.name
+                            string name = "";
+                            name = regex("(?<=" + b + Regex.Escape(".") + "name=).+", text.ToString());
+
+                            //Get b.menu.cpu.*.build.mcu 
+                            Regex r = new Regex("(?<=" + b + Regex.Escape(".") + "menu" + Regex.Escape(".") + "cpu" + Regex.Escape(".") + "[0-9a-zA-Z]+" + Regex.Escape(".") + "build" + Regex.Escape(".") + "mcu=).+");
+
+                            string mcu = "";
+                            string cpu = "";
+                            bool firstFlag = true;
+                            foreach (Match match in r.Matches(text.ToString()))
+                            {
+
+                                //Get cpu of that mcu
+                                string c = "";
+                                c = regex(b + Regex.Escape(".") + "menu" + Regex.Escape(".") + "cpu" + Regex.Escape(".") + "[0-9a-zA-Z]+" + Regex.Escape(".") + "build" + Regex.Escape(".") + "mcu=" + match.Value, text.ToString());
+
+                                string[] arrC = c.Split('.');
+
+                                if (firstFlag)
+                                    firstFlag = false;
+                                else
+                                {
+                                    mcu += ",";
+                                    cpu += ",";
+                                }
+                                    
+                                mcu += match.Value;
+                                cpu += arrC[3];
+                            }
+
+                            //Add to DataGrid
+                            this.dataGridView1.Rows.Add(port, b, name, pid, vid, sn, mcu, cpu);
+                        }
+                    }
+                }
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                MessageBox.Show("boards.txt not found: " + ex.ToString());
+            }
+            catch(System.IO.DirectoryNotFoundException ex)
+            {
+                MessageBox.Show("boards.txt not found: " + ex.ToString());
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("Exception while trying to access serial ports. Check if your account have admin rights: " + ex.ToString());
+                return;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadBoards();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtPort.Text=="" || txtBoard.Text=="" || selectedrowindex == -1)
+            {
+                MessageBox.Show("Select a device in the above list");
+            }else
+            {
+                Console.WriteLine(txtBoard.Text + " " + txtPort.Text + " " + comboCPU.Text + " " + comboMCU.Text);
+                this.Close();
+            }
+            
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try{
+                //https://stackoverflow.com/questions/7657137/datagridview-full-row-selection-but-get-single-cell-value
+                this.selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+
+                DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+
+                txtPort.Text = Convert.ToString(selectedRow.Cells["port"].Value);
+                txtBoard.Text = Convert.ToString(selectedRow.Cells["board"].Value);
+
+                this.comboCPU.Items.Clear();
+                this.comboMCU.Items.Clear();
+                string cpus = Convert.ToString(selectedRow.Cells["cpu"].Value);
+                string mcus = Convert.ToString(selectedRow.Cells["mcu"].Value);
+
+                string[] arrCPUs = cpus.Split(',');
+                foreach (string cpu in arrCPUs)
+                    comboCPU.Items.Add(cpu);
+
+                string[] arrMCUs = mcus.Split(',');
+                foreach (string mcu in arrMCUs)
+                    comboMCU.Items.Add(mcu);
+
+                comboCPU.SelectedIndex = 0;
+                comboMCU.SelectedIndex = 0;
+
+                //Hide comboCPU if no cpu selection is required
+                if (string.IsNullOrEmpty(cpus))
+                {
+                    comboCPU.Visible = false;
+                    lblCpu.Visible = false;
+                }
+                else
+                {
+                    comboCPU.Visible = true;
+                    lblCpu.Visible = true;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnIDE_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("ide");
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadBoards();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboCPU_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboMCU.SelectedIndex = comboCPU.SelectedIndex;
+        }
+    }
+}
